7e70bf2f6ae0d32c301801e6feba5d1be9ca877e
Sokpheng <sokpheng234@gmail.com>
2025-07-08 19:14:04 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Sokpheng Profolio',
+  description: 'Created by Sokpheng',
   generator: 'v0.dev',
 }
 
